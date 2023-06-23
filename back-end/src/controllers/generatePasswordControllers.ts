6871ba6c61dcb0e12e4a5ea8d9c437b4fa20b30d
codethi <docencia.thiago@gmail.com>
2023-06-23 19:19:57 -0300

    feat: docker api $ postgres

--- back-end/src/controllers/generatePasswordControllers.ts
+++ back-end/src/controllers/generatePasswordControllers.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from "express";
 import generatePasswordService from "../services/generatePasswordServices";
+import { ICreatePassword } from "interfaces/ICreatePassword";
 
 async function createPassword(req: Request, res: Response): Promise<Response> {
-  const { length } = req.body;
+  const { length } = req.body as ICreatePassword;
   try {
     const password = await generatePasswordService.createPassword(length);
     return res.status(201).send({ password });
